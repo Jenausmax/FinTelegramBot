b78a6d11aa1132ddc3e0ed0a28574689fb779adf
Jenausmax <mminmax@gmail.com>
2021-04-22 09:54:20 +0300

    Удален интерфей IResponder и класс ResponderServices. Удален класс KeyboardCollection.

--- FinBot/FinBot.App/Services/CommandService.cs
+++ FinBot/FinBot.App/Services/CommandService.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using FinBot.App.Keyboard;
+﻿using System.Collections.Generic;
 using FinBot.Domain.Interfaces;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
