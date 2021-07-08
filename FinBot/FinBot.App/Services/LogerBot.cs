4a0bba125da2901119abce149ea2cbf80732f252
Jenausmax <mminmax@gmail.com>
2021-07-08 22:45:00 +0300

    Добавлен интерфейс ILogerBot.

---
+++ FinBot/FinBot.App/Services/LogerBot.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FinBot.Domain.Interfaces;
+
+namespace FinBot.App.Services
+{
+    public class LogerBot : ILogerBot
+    {
+        public void WorkLogCreate(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ErrorLogCreate(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FinBot/FinBot.Domain/Interfaces/ILogerBot.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinBot.Domain.Interfaces
+{
+    public interface ILogerBot
+    {
+        void WorkLogCreate(string message);
+        void ErrorLogCreate(string message);
+    }
+}
